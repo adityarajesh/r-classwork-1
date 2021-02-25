7c0bc737d0856fe34d56d1326a9c5c353126c06f
adityarajesh <79098775+adityarajesh@users.noreply.github.com>
2021-02-25 14:37:49 +0400

    Create r-class1.R

---
+++ r-class1.R
@@ -0,0 +1,54 @@
+print("Hello, World!")
+2+3
+50000*42222
+b <- 19
+a = 2
+a+B
+a = b
+a==b
+(y = x ** 4)
+(x <- 2 + 3)
+(y = x ** 4)
+nummy <- c(2,3,4)
+nummy_int <- c(1L,2L,3L)
+typeof(nummy)
+typeof(nummy_int)
+is.numeric(nummy)
+is.numeric(nummy_int)
+is.double(nummy)
+types <- c("int","double","character")
+types
+length(types)
+is.numeric(types)
+is.character(types)
+logicals <- c(TRUE,F,TRUE,T, FALSE)
+logicals
+logicals = types 
+a <- c(2,3,4,"ink") 
+a
+type(a)
+money_in_chars <- c("20","35","33")
+typeof(money_in_chars)
+money_money <- as.logical(money_in_chars)
+money_money
+typeof(money_in_chars)
+typeof(money_money)
+> money_logical <- as.logical(money_money)
+> money_logical
+as.logical(c(15,20,25))
+numbers <- 1:5
+numbers <- c(numbers,8)
+numbers 
+numbers <- c(numbers,"A")
+numbers
+month.abb
+month.name
+month.abb[2]
+month.abb[2:3]
+days <- c("Mon","Tue","Wed")
+days
+week_end <- c("Sat","Sun")
+more_days <- c(days,"Thu","Fri",week_end)
+more_days
+Road <- c('car','bike', 'bus')
+Air <- c('flight'','rocket')
